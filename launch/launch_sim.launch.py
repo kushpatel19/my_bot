97bcc16b95a15ec276a2e8fa6141355f8f057cca
Your Name <you@example.com>
2023-07-12 13:46:23 -0400

    Added simulated lidar

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -14,8 +14,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    # Include the robot_state_publisher launch file, provided by my own package. Force sim time to be enabled
 
     package_name='my_bot'
     package_dir = get_package_share_directory(package_name)
@@ -55,6 +54,10 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         node_rviz,
+        DeclareLaunchArgument(
+        'world',
+        default_value=[os.path.join(package_dir, 'worlds','obstacles.world'), ''],
+        description='SDF world file'),
         gazebo,
         spawn_entity,
     ])
