c91e063139dc93fe259abd31f5735baa9b797c3c
Your Name <you@example.com>
2023-07-12 18:20:50 -0400

    Add ros2_control code

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -47,6 +47,18 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
     # Launch them all!
     return LaunchDescription([
@@ -58,4 +70,6 @@ def generate_launch_description():
         description='SDF world file'),
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
