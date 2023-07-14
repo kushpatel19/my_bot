15dc128667b6614b74c7ba0d266f3e2cb17d7333
Your Name <you@example.com>
2023-07-13 23:05:21 -0400

    Implemented sim_mode

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,8 +22,10 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true','use_ros2_control':'true'}.items()
     )
+
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     #Rviz configuration file path
     rviz = os.path.join(package_dir,'config' , 'drive_bot.rviz')
@@ -39,6 +41,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
 
